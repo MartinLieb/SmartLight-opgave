63c3a996fff4c23d555d96af69d43d14346b00b1
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-10-25 10:33:42 +0200

    opgave lavet

--- src/hu/hungarian/Main.java
+++ src/hu/hungarian/Main.java
@@ -1,14 +1,28 @@
 package hu.hungarian;
 
 
-import javafx.scene.paint.Color;
-import java.lang.reflect.Field;
-
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
+
         SmartLight s = new SmartLight();
+        s.turnUp();
+        System.out.println(s.getLightPercent());
+        s.turnUp();
+        System.out.println(s.getLightPercent());
+        s.turnUp();
+        System.out.println(s.getLightPercent());
+        s.turnUp();
+        System.out.println(s.getLightPercent());
+        s.turnUp();
+        System.out.println(s.getLightPercent());
+        s.turnUp();
+        System.out.println(s.getLightPercent());
+        s.turnUp();
+        System.out.println(s.getLightPercent());
         System.out.println(s.toString());
+
     }
+
 }
--- src/hu/hungarian/SmartLight.java
+++ src/hu/hungarian/SmartLight.java
@@ -2,8 +2,61 @@ package hu.hungarian;
 
 import javafx.scene.paint.Color;
 
+import static javafx.scene.paint.Color.WHITESMOKE;
+
 public class SmartLight {
 
+    short lightPercent;
+    Color color;
 
 
+    public SmartLight() {
+
+        System.out.println("Nyt smartlight objekt er lavet");
+
+        color = WHITESMOKE;
+        lightPercent = 0;
+
+
+    }
+
+    public void turnUp(){
+
+        if(lightPercent+20 >=100){
+            this.lightPercent = 100;
+        }
+        else{
+            this.lightPercent += 20;
+        }
+    }
+
+    public void turnDown(){
+
+        if(lightPercent-20 <=0){
+            this.lightPercent = 0;
+        }
+        else{
+            this.lightPercent -= 20;
+        }
+    }
+
+    public String toString(){
+        return this.lightPercent + " " + this.getColor();
+    }
+
+    public short getLightPercent() {
+        return lightPercent;
+    }
+
+    public void setLightPercent(short lightPercent) {
+        this.lightPercent = lightPercent;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
